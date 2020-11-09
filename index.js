2f9ada63219ea75ba751b9be0dbb317661ad23e9
sbleahlee <ewhaelle@gmail.com>
2020-11-09 12:24:10 +0000

    SHA256 추가

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const cookieParser = require('cookie-parser');
 const app = express();  
+const sha256 = require('js-sha256');
 
   
 app.use(bodyParser.urlencoded({ extended:  false  }));  
@@ -17,36 +18,45 @@ function checkData(name, pw, url){
     console.log('checkData');
     console.log(name, '/', pw, '/', url );
     console.log(Boolean(name), '/', Boolean(pw), '/', Boolean(url) );
+    var msg ;
 
     if(!name|name===''|!pw|pw===''){
-        console.log('유저명과 비밀번호는 필수');
-        return; //유저명과 비밀번호는 필수
+        msg = '유저명과 비밀번호는 필수';
+        return msg; //유저명과 비밀번호는 필수
     }
+    
+    //암호화를 시켜보자
+    var npw = sha256(pw);
+
 
     if (userInfo.hasOwnProperty(name)){             
-        if(pw !== userInfo[name].pw){
-            return; // 비밀번호 틀렸음
+        if(npw !== userInfo[name].pw){
+            msg = '비밀번호 틀림';
+            return msg; // 비밀번호 틀렸음
         } 
 
         if(!url||url[0]===''){
-            // if(userInfo[name].ytaddr===''||!userInfo[name].ytaddr){
-            //     return; // 저장된 URL도 없는데 새로 저장도 안 하다니!
-            // }
-            return;
+            if(userInfo[name].ytaddr===''||!userInfo[name].ytaddr){
+                msg = '저장된 URL도 없고 신규저장도 없고';
+            return msg;
+            }
+
 
         }else if(url[url.length-1] === userInfo[name].ytaddr){
-            return; // 저장된 값이나 이거나 똑같
+            msg = '저장된 URL이나 이거나 똑같스';
+            return msg; // 저장된 값이나 이거나 똑같
         }else{
             userInfo[name].ytaddr = url[url.length-1]; //기존url업데이트
         }
     }    else{ 
             if(!url||url[0]===''){
-                return; //유저명입력했는데 URL은 없어..
+                msg = '유저명 입력했는데 URL이 없네요';
+                return msg; //유저명입력했는데 URL은 없어..
             }
             //신규저장
             userInfo[name]= {
             "ytaddr" : url[url.length-1],
-            "pw" : pw
+            "pw" : npw
             }    
         }
 }
@@ -61,7 +71,7 @@ app.get('/',function (req,res) {
         name = req.cookies.uname;
         pw = req.cookies.upw;
         if(userInfo[name])
-            youtubeAddr = 'https://www.youtube.com/embed/'+userInfo[name];
+            youtubeAddr = 'https://www.youtu.be'+userInfo[name];
 
     }       
 
@@ -83,10 +93,16 @@ app.get('/userinfo',function (req,res) {
     var inpw = req.query.upw;
     var inurl = req.query.youtube_addr.split('/');
 
-    checkData(inname, inpw, inurl);
+    var checkMsg = checkData(inname, inpw, inurl);
 
-    if(!userInfo[inname]){
-        console.log('입력값에 대한 userInfo가 없다는 뜻');
+    // if(!userInfo[inname]){
+    //     console.log('입력값에 대한 userInfo가 없다는 뜻');
+    //     res.redirect('/');
+    //     return;
+    // }
+
+    if(checkMsg){
+        console.log(checkMsg);
         res.redirect('/');
         return;
     }
